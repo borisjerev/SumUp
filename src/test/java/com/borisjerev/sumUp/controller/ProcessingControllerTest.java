5760aa0c005f2d3ba4ad8e544112e3eff6c046ed
Boris Jerev <borisjerev@gmail.com>
2020-06-21 00:20:40 +0300

    Added controller test

---
+++ src/test/java/com/borisjerev/sumUp/controller/ProcessingControllerTest.java
@@ -0,0 +1,40 @@
+package com.borisjerev.sumUp.controller;
+
+import com.borisjerev.sumUp.model.TasksRequest;
+import com.borisjerev.sumUp.service.api.OrderTasksServiceInterface;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@AutoConfigureMockMvc
+@WebMvcTest(controllers = ProcessingController.class)
+public class ProcessingControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private OrderTasksServiceInterface orderTaskService;
+
+    @Test
+    public void shouldReturnBadRequest() throws Exception {
+        TasksRequest tasksRequest = new TasksRequest();
+        this.mockMvc.perform(get("/tasks")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(new ObjectMapper().writeValueAsString(tasksRequest)))
+                .andExpect(status().isBadRequest());
+    }
+}
