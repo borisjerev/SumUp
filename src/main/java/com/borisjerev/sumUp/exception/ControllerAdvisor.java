4883e167dc13e267660eb44d969cd32fd65d6f1f
Boris Jerev <borisjerev@gmail.com>
2020-06-20 15:57:41 +0300

    add timestamp to error handling

--- src/main/java/com/borisjerev/sumUp/exception/ControllerAdvisor.java
+++ src/main/java/com/borisjerev/sumUp/exception/ControllerAdvisor.java
@@ -5,10 +5,19 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 @ControllerAdvice
 public class ControllerAdvisor {
     @ExceptionHandler(BadTasksRequestException.class)
-    public ResponseEntity<String> handleBasTasksRequestException(BadTasksRequestException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    public ResponseEntity<Map> handleBasTasksRequestException(BadTasksRequestException ex) {
+
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("error", ex.getMessage());
+
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
     }
 }
